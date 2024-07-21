7a539c1a6ded16e2bd63dd3c4924b06a6a85aa93
Stroy <114012059+R-LaRoi@users.noreply.github.com>
2024-07-21 19:08:02 -0400

    Update conn.mjs

--- db/conn.mjs
+++ db/conn.mjs
@@ -1,14 +1,23 @@
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const client = new MongoClient(process.env.ATLAS_URI);
 
-let conn;
-try {
-  conn = await client.connect();
-} catch (e) {
-  console.error(e);
+async function connectMongo() {
+  let conn;
+
+  try {
+    conn = await client.connect();
+    console.log("Connected to MongoDB successfully");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+
+  const db = conn.db("sample_training");
+  return db;
 }
 
-let db = conn.db("sample_training");
-
+const db = await connectMongo();
 export default db;
